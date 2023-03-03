b1ea78aab7381e1a5b4c393d476c3a852ca98046
Ümit Yaşar Kalkan <uykalkan@gmail.com>
2023-03-03 11:54:56 +0300

    add fonts settings and fonts, font helpers

---
+++ src/constants/globalStyles.ts
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  fullAbsolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  rubik300: {
+    fontFamily: 'Rubik-Light',
+  },
+  rubik400: {
+    fontFamily: 'Rubik-Regular',
+  },
+  rubik500: {
+    fontFamily: 'Rubik-Medium',
+  },
+  rubik700: {
+    fontFamily: 'Rubik-Bold',
+  },
+  rubik800: {
+    fontFamily: 'Rubik-ExtraBold',
+  },
+  sfProText400: {
+    fontFamily: 'SFProText-Regular',
+  },
+  sfProText700: {
+    fontFamily: 'SFProText-Bold',
+  },
+});
